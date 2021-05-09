b9663e62eeb6d304b69833483ced1eb91103db05
T357A <Sean@CurrentElectrical.Services>
2021-05-08 20:11:54 -0400

    Closer

--- Temp_Converter.py
+++ Temp_Converter.py
@@ -1,6 +1,6 @@
 
 def k_calc():
-    print(temp, scale, ('˚ is: '))
+    print(temp, temp_calc, ('˚ is: '))
     kel_cel = temp - 273.15
     kel_fah = ((temp - 273.15) * 1.8) + 32
     kel_ran = temp * 1.8
@@ -10,7 +10,7 @@ def k_calc():
 
 
 def r_calc():
-    print(temp, scale, ('˚ is: '))
+    print(temp, temp_calc, ('˚ is: '))
     ran_fah = temp - 459.67
     ran_cel = (temp - 491.67)*1.8
     ran_kel = temp * 1.8
@@ -20,7 +20,7 @@ def r_calc():
 
 
 def f_calc():
-    print(temp, scale, ('˚ is: '))
+    print(temp, temp_calc, ('˚ is: '))
     fah_cel = (temp - 32) * 1.8
     fah_ran = temp + 459.76
     fah_kel = ((temp - 32) * 1.8) + 273.15
@@ -30,7 +30,7 @@ def f_calc():
 
 
 def c_calc():
-    print(temp, scale, ('˚ is: '))
+    print(temp, temp_calc, ('˚ is: '))
     cel_fah = (temp * 1.8) + 32
     cel_kel = temp + 273.15
     cel_ran = (temp * 1.8) + 491.67
@@ -38,38 +38,32 @@ def c_calc():
     print(cel_fah, ' ˚F')
     print(cel_ran, ' ˚R')
 
-
+# Deciding what function to use to calculate remaining values
 def temp_calc():
+    global scale
+    scale = input(str(
+        "What temprature scale did you want to convert from? F= Fahrenheit, C=Celsius, R= Rankine, K:+ Kelvin: "))
+    scale = scale.upper()
     while True:
         try:
-            if scale == 'K':
+            if scale is 'K':
                 k_calc()
-            elif scale == 'R':
+            if scale is 'R':
                 r_calc()
-            elif scale == 'F':
+            if scale is 'F':
                 f_calc()
-            elif scale == 'C':
+            if scale is 'C':
                 c_calc()
         except:
             print('Please select a proper temperature scale: ')
             continue
 
-
+# User input to star program
 while True:
     try:
         temp = float(
             input('What is the numerical temperature that you would like converted?: '))
-        break
-    except ValueError:
-        print('Please check your input: ')
-        continue
-
-while True:
-    try:
-        scale = input(str(
-            "What temprature scale did you want to convert from? F= Fahrenheit, C=Celsius, R= Rankine, K:+ Kelvin: "))
-        scale = scale.upper()
         temp_calc()
     except ValueError:
-        print('Please choose a valid temperature scale: ')
+        print('Please check your input: ')
         continue
